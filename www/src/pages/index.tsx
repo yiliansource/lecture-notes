598749863fdd85f3b2027ad8ed9f33382c886977
Ian Hornik <yiliansource@gmail.com>
2025-03-03 18:02:33 +0100

    refine frontend

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -81,10 +81,20 @@ export const getStaticProps: GetStaticProps<{
 export default function Home({ lectureNotes }: InferGetStaticPropsType<typeof getStaticProps>) {
     return (
         <div className={`${geistSans.variable} ${geistMono.variable} font-[family-name:var(--font-geist-sans)]`}>
-            <div className="flex flex-col">
-                {lectureNotes.map((l) => (
-                    <LectureNotesItem {...l} key={l.id} />
-                ))}
+            <div className="flex flex-col mx-auto w-full min-h-screen max-w-4xl">
+                <header className="pt-16 pb-8 select-none">
+                    <h1 className="text-4xl font-bold">Vorlesungsmitschriften</h1>
+                    <p className="text-xl font-semibold">Ian Hornik</p>
+                </header>
+                <div className="grow flex flex-col divide-y divide-gray-200">
+                    {lectureNotes.map((l) => (
+                        <LectureNotesItem {...l} key={l.id} />
+                    ))}
+                </div>
+                <footer className="py-8 flex justify-between select-none text-gray-500 ">
+                    <p>Eine Ansammlung an Vorlesungsmitschriften.</p>
+                    <p>Copyright &copy; {new Date().getFullYear()} Ian Hornik</p>
+                </footer>
             </div>
         </div>
     );
@@ -94,17 +104,35 @@ function LectureNotesItem({ id, title, lecturer, semester, lastChanged }: Lectur
     const lastChangedDate = new Date(lastChanged);
 
     return (
-        <div className="mb-2">
-            <p className="text-xl">
-                <a href={"/documents/" + id + ".pdf"} target="_blank">
-                    {title}
-                </a>
-            </p>
-            <p>{lecturer}</p>
-            <p>{semester}</p>
-            <p>
-                {lastChangedDate.getDate()}.{lastChangedDate.getMonth() + 1}.{lastChangedDate.getFullYear()}
-            </p>
+        <div className="py-4">
+            <div className="flex flex-col">
+                <div className="flex flex-row gap-3">
+                    <span>
+                        <a className="hover:underline" href={"/documents/" + id + ".pdf"} target="_blank">
+                            {title}
+                        </a>
+                    </span>
+                    {semester && (
+                        <span className="inline-flex items-center px-2 py-1 text-xs font-medium text-blue-700 bg-blue-50 ring-1 ring-inset ring-blue-700/10 rounded-md">
+                            {semester}
+                        </span>
+                    )}
+                </div>
+                {lecturer && (
+                    <div>
+                        <span className="text-sm">{lecturer}</span>
+                    </div>
+                )}
+                <div>
+                    <span className="text-sm text-gray-600">
+                        Aktualisiert:{" "}
+                        {new Intl.DateTimeFormat("de-DE", {
+                            dateStyle: "full",
+                        }).format(lastChangedDate)}
+                    </span>
+                </div>
+            </div>
+            <div></div>
         </div>
     );
 }
