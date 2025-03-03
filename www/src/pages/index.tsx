8d14eb89c3dfe32f0be326a8120341cc6bd2af2f
Ian Hornik <yiliansource@gmail.com>
2025-03-03 18:17:29 +0100

    make responsive, add build caching

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -81,9 +81,9 @@ export const getStaticProps: GetStaticProps<{
 export default function Home({ lectureNotes }: InferGetStaticPropsType<typeof getStaticProps>) {
     return (
         <div className={`${geistSans.variable} ${geistMono.variable} font-[family-name:var(--font-geist-sans)]`}>
-            <div className="flex flex-col mx-auto w-full min-h-screen max-w-4xl">
-                <header className="pt-16 pb-8 select-none">
-                    <h1 className="text-4xl font-bold">Vorlesungsmitschriften</h1>
+            <div className="px-3 flex flex-col mx-auto w-full min-h-screen max-w-4xl">
+                <header className="pt-12 lg:pt-16 pb-4 lg:pb-8 select-none">
+                    <h1 className="text-3xl lg:text-4xl font-bold">Vorlesungsmitschriften</h1>
                     <p className="text-xl font-semibold">Ian Hornik</p>
                 </header>
                 <div className="grow flex flex-col divide-y divide-gray-200">
@@ -91,7 +91,7 @@ export default function Home({ lectureNotes }: InferGetStaticPropsType<typeof ge
                         <LectureNotesItem {...l} key={l.id} />
                     ))}
                 </div>
-                <footer className="py-8 flex justify-between select-none text-gray-500 ">
+                <footer className="py-4 lg:py-8 flex flex-col lg:flex-row justify-between select-none text-gray-500 ">
                     <p>Eine Ansammlung an Vorlesungsmitschriften.</p>
                     <p>Copyright &copy; {new Date().getFullYear()} Ian Hornik</p>
                 </footer>
